bef1950b78d3860004ea2e2591cc9c54d2932ed2
Brandon Gonzalez <brajgon@gmail.com>
2024-02-29 20:34:40 -0700

    server/controller.js

--- server/controller.js
+++ server/controller.js
@@ -2,12 +2,17 @@ module.exports = {
 
     getCompliment: (req, res) => {
         const compliments = ["Gee, you're a smart cookie!", "Cool shirt!", "Your Javascript skills are stellar."];
-      
-        // choose random compliment
         let randomIndex = Math.floor(Math.random() * compliments.length);
         let randomCompliment = compliments[randomIndex];
-      
         res.status(200).send(randomCompliment);
-    }
+    },
 
-}
+    getFortune: (req, res) => {
+        const fortunes = ["You love peace", "A Smooth Long journey! Great expectaions", "A truly rich life contains love and art in abundance"];
+        let randoIndex = Math.floor(Math.random() * fortunes.length);
+        let randomFortune = fortunes[randoIndex];
+        res.status(200).send(randomFortune);
+    },
+
+    
+};
--- server/index.js
+++ server/index.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const cors = require("cors");
-
 const app = express();
+const randomStuff = require('./controller')
+const { getCompliment, getFortune } = randomStuff
 
 app.use(cors());
-
 app.use(express.json());
 
-const { getCompliment } = require('./controller')
-
 app.get("/api/compliment", getCompliment);
+app.get("/api/fortunes", getFortune);
 
 app.listen(4000, () => console.log("Server running on 4000"));
+
